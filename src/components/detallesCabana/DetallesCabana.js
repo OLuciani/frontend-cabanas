b9fb426e796561e53172cd9742cfd0c259197ddd
OLuciani <lucianioscar1@gmail.com>
2023-11-26 22:43:24 -0300

    Correcciones en DEtallesCabana.css y en ModalReservar.css

--- src/components/detallesCabana/DetallesCabana.js
+++ src/components/detallesCabana/DetallesCabana.js
@@ -87,11 +87,6 @@ const DetallesCabana = () => {
 
 export default DetallesCabana; */
 
-
-
-
-
-
 /* import React, { useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import { useContext, useEffect } from "react";
@@ -192,9 +187,6 @@ const DetallesCabana = () => {
 
 export default DetallesCabana; */
 
-
-
-
 import React, { useState, useEffect, useLayoutEffect } from "react";
 import { Link, useParams, useLocation } from "react-router-dom";
 import { useContext } from "react";
@@ -234,18 +226,18 @@ const DetallesCabana = () => {
     if (shouldFetchData) {
       // Realiza una llamada a la API para obtener los datos actualizados
       //fetch('http://localhost:5005/api/list', {
-      fetch('https://cabanas-backend.onrender.com/api/list', {
-        method: 'GET',
+      fetch("https://cabanas-backend.onrender.com/api/list", {
+        method: "GET",
         headers: {
-          'Authorization': `Bearer ${token}` // Agrega el token de autorización
-        }
+          Authorization: `Bearer ${token}`, // Agrega el token de autorización
+        },
       })
-      .then((res) => res.json())
-      .then((allCabañas) => {
-        infoBD.setData(allCabañas);
-        setShouldFetchData(false); // Establece shouldFetchData a false después de actualizar los datos
-      })
-      .catch((error) => console.log(error));
+        .then((res) => res.json())
+        .then((allCabañas) => {
+          infoBD.setData(allCabañas);
+          setShouldFetchData(false); // Establece shouldFetchData a false después de actualizar los datos
+        })
+        .catch((error) => console.log(error));
     }
   }, [shouldFetchData, infoBD, token]);
 
@@ -254,15 +246,17 @@ const DetallesCabana = () => {
     setShouldFetchData(true);
     //console.log("Se ha navegado a la vista de Cabanas");
   }, [location]);
-  
+
   //const infoBD = useContext(Context);
   const { _id } = useParams();
   let cabaña = infoBD.data.find((cab) => cab._id === _id);
   let images = cabaña.url_images;
 
   //const [ocultarContenedor, setOcultarContenedor] = useState("");
-  const [mostrarComponenteReservar, setMostrarComponenteReservar] = useState(false);
-  const [mostrarModalUsuarioNoRegistrado, setMostrarModalUsuarioNoRegistrado] = useState("");
+  const [mostrarComponenteReservar, setMostrarComponenteReservar] =
+    useState(false);
+  const [mostrarModalUsuarioNoRegistrado, setMostrarModalUsuarioNoRegistrado] =
+    useState("");
 
   const onClickOcultar = () => {
     const token = localStorage.getItem("token");
@@ -276,7 +270,7 @@ const DetallesCabana = () => {
 
   const onClickOcultarModalUNR = () => {
     setMostrarModalUsuarioNoRegistrado("");
-  }
+  };
 
   useLayoutEffect(() => {
     const handleBeforeUnload = () => {
@@ -297,12 +291,14 @@ const DetallesCabana = () => {
 
   const onClickOcultarReservar = () => {
     setMostrarComponenteReservar(false);
-  }
+  };
   return (
     <>
       <NavBar />
 
-      <div className={`contenedor-detalles-cabana`}> {/*En esta clase iba esto ${ocultarContenedor} */}
+      <div className={`contenedor-detalles-cabana`}>
+        {" "}
+        {/*En esta clase iba esto ${ocultarContenedor} */}
         <Container className="carousel-container">
           <Carousel fade>
             {images.map((image, index) => (
@@ -314,34 +310,34 @@ const DetallesCabana = () => {
                   alt={`Imagen ${index}`}
                 />
                 <Carousel.Caption className="carousel-caption">
-                  <p>{cabaña.name} ({index + 1})</p>
+                  <p>
+                    {cabaña.name} ({index + 1})
+                  </p>
                 </Carousel.Caption>
               </Carousel.Item>
             ))}
           </Carousel>
         </Container>
+        {/* Mostrar los botones "Editar cabaña" y "Eliminar cabaña"solo si el usuario es administrador */}
+        {localStorage.getItem("role") === "admin" && (
+          <div className="contenedor-botones-admin">
+            <div className="botones-admin">
+              <Link to={`/updateCabana/${_id}`}>
+                <button className="boton-editar">Editar cabaña</button>
+              </Link>
 
-         {/* Mostrar los botones "Editar cabaña" y "Eliminar cabaña"solo si el usuario es administrador */}
-         {localStorage.getItem('role') === 'admin' && ( 
-            <div className="contenedor-botones-admin">
-              <div className="botones-admin">
-                <Link to={`/updateCabana/${_id}`}>
-                  <button className="boton-editar">Editar cabaña</button>
-                </Link>
+              <Link to={`/verFechas/${cabaña._id}`}>
+                <button className="boton-fechas-disponibles">
+                  Fechas Disponibles
+                </button>
+              </Link>
 
-                <Link to={`/verFechas/${cabaña._id}`}>
-                  <button className="boton-fechas-disponibles">Fechas Disponibles</button>
-                </Link>
-
-                <Link to={`/deleteCabana/${_id}`}>
-                  <button className="boton-eliminar">Eliminar cabaña</button>
-                </Link>
-              </div>
+              <Link to={`/deleteCabana/${_id}`}>
+                <button className="boton-eliminar">Eliminar cabaña</button>
+              </Link>
             </div>
-          )}
-       
-
-
+          </div>
+        )}
         {/* {infoBD.startDate !== "" && infoBD.endDate !== "" ? (
           <button className="boton-fechas-disponibles" onClick={onClickOcultar}>
             Iniciar Reserva
@@ -351,45 +347,38 @@ const DetallesCabana = () => {
             <p className="boton-fechas-disponibles">Fechas Disponibles</p>
           </Link>
         )} */}
-
-        {infoBD.startDate !== "" && infoBD.endDate !== "" &&  
-          (<button className="boton-fechas-disponibles" onClick={onClickOcultar}>
+        {infoBD.startDate !== "" && infoBD.endDate !== "" && (
+          <button className="boton-fechas-disponibles" onClick={onClickOcultar}>
             Iniciar Reserva
-          </button>)
-        }
-
-       {/*  {
+          </button>
+        )}
+        {/*  {
           localStorage.getItem("role") === "admin" && 
           (<Link to={`/verFechas/${cabaña._id}`}>
             <p className="boton-fechas-disponibles">Fechas Disponibles</p>
           </Link>)
         } */}
-
         <div className="contenedor-items-detalles-cabaña">
           <p className="price-detalles-cabaña">
             <b>Cantidad de habitaciones:</b> {cabaña.rooms}
           </p>
         </div>
-
         <div className="contenedor-items-detalles-cabaña">
           <p className="description-detalles-cabaña">
             <b>Descripción:</b> {cabaña.description}
           </p>
         </div>
-
         <div className="contenedor-items-detalles-cabaña">
           <p className="services-detalles-cabaña">
             <b>Servicios:</b> {cabaña.services}
           </p>
         </div>
-
         <div className="contenedor-items-detalles-cabaña">
           <p className="price-detalles-cabaña">
             <b>Precio por día:</b> $ {cabaña.price}
           </p>
         </div>
-
-      {/* {infoBD.startDate !== "" && infoBD.endDate !== "" ? (
+        {/* {infoBD.startDate !== "" && infoBD.endDate !== "" ? (
         <button className="boton-fechas-disponibles" onClick={onClickOcultar}>
         Iniciar Reserva
         </button>
@@ -398,25 +387,36 @@ const DetallesCabana = () => {
           <button className="boton-fechas-disponibles">Fechas Disponibles</button>
           </Link>
         )} */}
-
         {
           <div className="container-modal-usuario-no-registrado">
-            <div className={`usuario-no-registrado ${mostrarModalUsuarioNoRegistrado}`}>
-              <p onClick={onClickOcultarModalUNR} className={`close-usuario-no-registrado`}>X</p>
+            <div
+              className={`usuario-no-registrado ${mostrarModalUsuarioNoRegistrado}`}
+            >
+              <p
+                onClick={onClickOcultarModalUNR}
+                className={`close-usuario-no-registrado`}
+              >
+                X
+              </p>
 
               <ModalUsuarioNoRegistrado />
             </div>
           </div>
         }
-
-      {mostrarComponenteReservar 
-      ? <div className="content-reservar">
-          <div className="reservar">
-            <p onClick={onClickOcultarReservar} className="close-confirmar-fechas">X</p>
-            <ModalReservar />
+        {mostrarComponenteReservar ? (
+          <div className="content-reservar">
+            <div className="reservar">
+              <p
+                onClick={onClickOcultarReservar}
+                className="close-confirmar-fechas"
+              >
+                X
+              </p>
+              <ModalReservar />
+            </div>
           </div>
-      </div> : null}
-        </div>
+        ) : null}
+      </div>
 
       <SideBar />
 
@@ -426,4 +426,3 @@ const DetallesCabana = () => {
 };
 
 export default DetallesCabana;
-
